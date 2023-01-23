9f4463775ff47091ea1c3892e473c1bc9d84c371
Danil <pingvin5332@gmail.com>
2023-01-24 00:05:28 +0300

    Films

--- src/main/java/com/example/cinema/Window3.java
+++ src/main/java/com/example/cinema/Window3.java
@@ -11,8 +11,8 @@ public class Window3 extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Window3.class.getResource("Window3.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 800, 400);
-        stage.setTitle("Film");
+        Scene scene = new Scene(fxmlLoader.load(), 800, 600);
+        stage.setTitle("Films");
         stage.setScene(scene);
         stage.show();
     }
