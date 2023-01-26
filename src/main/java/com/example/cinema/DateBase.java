bba724e33cd3e556582cea11259c9bd5251d4749
Danil <pingvin5332@gmail.com>
2023-01-26 12:28:34 +0300

    Refactor

--- src/main/java/com/example/cinema/DateBase.java
+++ src/main/java/com/example/cinema/DateBase.java
@@ -1,10 +1,12 @@
 package com.example.cinema;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-import java.sql.ResultSet;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.sql.*;
+import java.util.Properties;
+
 public class DateBase extends Configs {
     Connection dbConnection;
 
@@ -58,4 +60,96 @@ public class DateBase extends Configs {
 
         return resSet;
     }
+    public static void main(String[] args) {
+        try{
+            String url = "jdbc:mysql://localhost/cinema?serverTimezone=Europe/Moscow&useSSL=false";
+            String username = "root";
+            String password = "1379248650";
+            Class.forName("com.mysql.cj.jdbc.Driver").getDeclaredConstructor().newInstance();
+
+            try (Connection conn = DriverManager.getConnection(url, username, password)){
+
+                Statement statement = conn.createStatement();
+
+                ResultSet resultSet = statement.executeQuery("SELECT * FROM users");
+                while(resultSet.next()){
+
+                    int id = resultSet.getInt(1);
+                    String name = resultSet.getString(2);
+                    String login = resultSet.getString(3);
+                    String pass = resultSet.getString(4);
+                    String country = resultSet.getString(5);
+                    String city = resultSet.getString(6);
+                    System.out.printf("%d. %s: %s; %s (%s, %s)\n", id, name, login, pass, country, city);
+
+                }
+            }
+        }
+        catch(Exception ex){
+            System.out.println("Connection failed...");
+
+            System.out.println(ex);
+        }
+
+        //java -classpath C:\Editor Files (x86)\MySQL\Connector J 8.0\mysql-connector-j-8.0.31.jar;D:\Projects\IntelliJIDEAProjects\Cinema\Cinema\src\main\java\com\example\cinema\Editor.java
+        try{
+            String url = "jdbc:mysql://localhost/cinema?serverTimezone=Europe/Moscow&useSSL=false";
+            String username = "root";
+            String password = "1379248650";
+
+            // работа с базой данных
+            Class.forName("com.mysql.cj.jdbc.Driver").getDeclaredConstructor().newInstance();
+            System.out.println("Connection succesfull!");
+
+            // команда создания таблицы
+            String sqlCommand = "CREATE TABLE films (Id INT PRIMARY KEY AUTO_INCREMENT)";
+
+
+            try(Connection conn = DriverManager.getConnection(url, username, password)) {
+
+                // создание таблицы
+                Statement statement = conn.createStatement();
+
+                statement.executeUpdate(sqlCommand);
+
+                //Добавление в таблицу
+                int rows = statement.executeUpdate("INSERT users(FirstName, Login, Password, Country, City) VALUES ('Админ', 'admin', 1111, 'Россия', 'Пенза')," +
+                "('Денис', 'Denis', 1379, 'Россия', 'Пенза'), ('Данил', 'Danil', 1234, 'Россия', 'Пенза')");
+                System.out.printf("Added %d rows", rows);
+
+
+                //Редактирование таблицы
+
+                 int red = statement.executeUpdate("UPDATE Products SET Price = Price - 5000");
+                 System.out.printf("Updated %d rows", red);
+
+
+                //Удаление
+                 int del = statement.executeUpdate("DELETE FROM Products WHERE Id = 3");
+                 System.out.printf("%d row(s) deleted", del);
+
+                 System.out.println("Database has been created!");
+            }
+
+        }
+        catch(Exception ex){
+            System.out.println("Connection failed...");
+
+            System.out.println(ex);
+        }
+    }
+    public static Connection getConnection() throws SQLException, IOException {
+
+        Properties props = new Properties();
+        try(InputStream in = Files.newInputStream(Paths.get("database.properties"))){
+            props.load(in);
+        }
+        String url = props.getProperty("url");
+        String username = props.getProperty("username");
+        String password = props.getProperty("1379248650");
+
+        return DriverManager.getConnection(url, username, password);
+    }
 }
+
+
